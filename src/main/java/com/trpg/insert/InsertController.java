689965daf6546a79b88eefcf2b12e2dd22e8b3ec
shimoe <nagonago561@yahoo.co.jp>
2016-12-03 23:08:10 +0900

    ファイル読み込み機能実装

--- src/main/java/com/trpg/insert/InsertController.java
+++ src/main/java/com/trpg/insert/InsertController.java
@@ -1,10 +1,20 @@
 package com.trpg.insert;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -22,27 +32,51 @@ public class InsertController {
 
 	@Autowired
 	NamedParameterJdbcTemplate jdbcTemplate;
+	
+    @Autowired
+    ResourceLoader resourceLoader;
 
 	@RequestMapping("/top")
 	public String home(Model model) {
-		//logger.info("RegistController called", locale);
+		// logger.info("RegistController called", locale);
 		SqlParameterSource param = new MapSqlParameterSource();
 		Integer result = jdbcTemplate.queryForObject("select count(*) from scenario", param, Integer.class);
 		
+		String filepath = "static/scenario/001.txt"; // src/main/resources 配下の相対パス
+        Resource resource = resourceLoader.getResource("classpath:" + filepath);
+        String name = "null";
+		try {
+			name = resource.getFile().toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		List<String> lines;
+		StringBuilder sb = new StringBuilder();
+		try {
+			lines = Files.readAllLines(Paths.get(name), StandardCharsets.UTF_8);
+			lines.stream().forEach(line -> {
+				sb.append(line).append("\n");
+			});
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 		InsertForm form = new InsertForm();
-		form.setInnerText("# Marked in browser\n\nRendered by **insertForm**." + result);
+		form.setInnerText(sb.toString());
 		model.addAttribute("insertForm", form);
-		
+
 		return "insert/top";
 	}
 
-//	@RequestMapping(value = "confirm", method = RequestMethod.GET)
-//	public String home2(InsertForm form) {
-//	//	String name = form.getName();
-//		//logger.info("name is " + name);
-//
-//		return "insert/confirm";
-//	}
+	// @RequestMapping(value = "confirm", method = RequestMethod.GET)
+	// public String home2(InsertForm form) {
+	// // String name = form.getName();
+	// //logger.info("name is " + name);
+	//
+	// return "insert/confirm";
+	// }
 
 }
