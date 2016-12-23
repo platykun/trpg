562ba52920618cc833825543664d1aa810221f50
platykun <nagonago561@yahoo.co.jp>
2016-12-23 10:24:04 +0900

    キャラクター修正画面項目追加

--- src/main/resources/static/js/characterInput.js
+++ src/main/resources/static/js/characterInput.js
@@ -60,19 +60,25 @@ function setAbilityRandom() {
 	$('#pow').val(rollDice(3, 6));
 	$('#edu').val(rollDice(3, 6) + 3);
 }
-function createSkillSelect(){
-    var objects = document.getElementsByClassName("skillSelect");
-    for(var i = 0, len = objects.length; i < len; i++){
-        createSkillSelectOption(objects.item(i));
-    }
+function createSkillSelect() {
+	var skillList = [ '-', '言いくるめ', '医学', '運転（自動車）', '応急手当', 'オカルト', '回避', '化学',
+			'鍵開け', '隠す', '隠れる', '機械修理', '聞き耳', 'クトゥルフ神話', '芸術', '経理', '考古学',
+			'コンピューター', '忍び歩き', '写真術', '重機械操作', '乗馬', '信用', '心理学', '人類学', '水泳',
+			'制作1', '制作2', '制作3', '精神分析', '生物学', '説得', '操縦1', '操縦2', '操縦3',
+			'地質学', '跳躍', '追跡', '電気修理', '電子工学', '天文学', '投擲', '登攀', '図書館',
+			'ナビゲート', '値切り', '博物学', '物理学', '変装', '法律', '他の言語1', '他の言語2', '母国語',
+			'ﾏｰｼｬﾙｱｰﾂ', '目星', '薬学', '歴史1', '歴史2', 'サブマシンガン', 'ショットガン', 'マシンガン',
+			'ライフル', 'キック', '組み付き', 'こぶし', '頭突き', '近接戦1', '近接戦2', '火器1', '火器2',
+			'火器3', '火器4', '火器5', '火器6' ];
+	var objects = document.getElementsByClassName("skillSelect");
+	for (var i = 0, len = objects.length; i < len; i++) {
+		for (var j = 0, sLen = skillList.length; j < sLen; j++) {
+			var option = document.createElement('option');
+			option.textContent = skillList[j];
+			objects.item(i).appendChild(option);
+		}
+	}
 }
-function createSkillSelectOption(object){
-    var option = document.createElement('option');
-    option.textContent = "hoge";
-    var objects = document.getElementsByClassName("skillSelect");
-    object.appendChild(option);
-}
-
 
 $('#int').keyup(function() {
 	setIdea();
