5bbb9f7983d1ac890ae3e2c53b94b3f4e62cabe7
platykun <nagonago561@yahoo.co.jp>
2017-01-17 00:37:29 +0900

    シーン表示機能できているところまでで一旦コミット

--- src/main/java/com/trpg/scenario/ScenarioDetail.java
+++ src/main/java/com/trpg/scenario/ScenarioDetail.java
@@ -1,6 +1,7 @@
 package com.trpg.scenario;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.trpg.entity.CharacterInfo;
 import com.trpg.entity.Scenario;
@@ -26,7 +27,7 @@ public class ScenarioDetail {
 	/** 概要 */
 	private String outline;
 
-	/** 推奨人数　*/
+	/** 推奨人数 */
 	private String recommendPlayer;
 
 	/** 推定プレイ時間 */
@@ -34,25 +35,57 @@ public class ScenarioDetail {
 
 	/** 内容 */
 	private String text;
-	
+
 	/** NPCのリスト */
 	private List<CharacterInfo> npcs;
-	
+
 	/** 敵のリスト */
 	private List<CharacterInfo> enemies;
 
 	/** アイテムのリスト */
 	private List<ItemForm> items;
 
-	/** シーンのリスト */
-	private List<SceneForm> scenes;
+	/** 導入シーンのリスト */
+	private List<SceneForm> introductionScenes;
 
-	public ScenarioDetail(Scenario scenario){
-		this.title=scenario.getTitle();
+	/** イベントシーンのリスト */
+	private List<SceneForm> eventScenes;
+
+	/** 場所シーンのリスト */
+	private List<SceneForm> placeScenes;
+
+	/** 時間シーンのリスト */
+	private List<SceneForm> timeScenes;
+
+	/** 戦闘シーンのリスト */
+	private List<SceneForm> combatScenes;
+
+	/** 結末シーンのリスト */
+	private List<SceneForm> endingScenes;
+	
+	/** シーンの全量リスト */
+	private List<SceneForm> allScenes;
+
+	public ScenarioDetail(Scenario scenario) {
+		this.title = scenario.getTitle();
 		this.outline = scenario.getOutline();
 		this.recommendPlayer = scenario.getRecommendPlayer();
 		this.time = scenario.getTime();
 		this.text = scenario.getText();
+	}
+
+	/**
+	 * シーンを取得して、各シーンへ格納する。
+	 */
+	public void setScenes(List<SceneForm> scenes) {
+		//TODO: 表示する用にシーンごとにリストを切り分けしているが、idとtitleのみ格納できさえすれば良い
+		this.introductionScenes = scenes.stream().filter(s -> s.getSceneGroup() == 10).collect(Collectors.toList());
+		this.eventScenes = scenes.stream().filter(s -> s.getSceneGroup() == 20).collect(Collectors.toList());
+		this.placeScenes = scenes.stream().filter(s -> s.getSceneGroup() == 30).collect(Collectors.toList());
+		this.timeScenes = scenes.stream().filter(s -> s.getSceneGroup() == 40).collect(Collectors.toList());
+		this.combatScenes = scenes.stream().filter(s -> s.getSceneGroup() == 50).collect(Collectors.toList());
+		this.endingScenes = scenes.stream().filter(s -> s.getSceneGroup() == 50).collect(Collectors.toList());
+		this.allScenes = scenes;
 	}
 
 }
